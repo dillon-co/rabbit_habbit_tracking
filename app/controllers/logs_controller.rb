6d4e6c0cf2e328881aef66d4d8fead9f888c4374
Dillon Cortez <dilloncortez@gmail.com>
2022-04-21 19:44:16 -0500

    another param fix

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,17 +3,15 @@ class LogsController < ApplicationController
   end
 
   def index
-    @logs = Task.includes(:logs).find(params[:id]).logs.all
+    @logs = Task.includes(:logs).find(params[:task_id]).logs.all
   end
 
   def new
-    # puts "\n\n\n #{params} \n\n\n"
     @task = Task.find(params[:task_id])
     @log = @task.logs.new
   end
 
   def create
-    puts "\n\n\n #{params} \n\n\n"
     log = Task.find(params[:task_id]).logs.new(log_params)
     if log.save
       log_redirect_path = log.gave_reward? ? reward_path : root_path
